103da223692618ea061fcff1fd07a3d5623df4e0
landinij0040 <ijlandin@miners.utep.edu>
2021-10-20 13:56:08 -0600

    Init Coommit

---
+++ admin.php
@@ -0,0 +1,11 @@
+<?php
+    // Making global php connection
+//    $host = "";
+//    $db = "";
+//    $username = "";
+//    $password = "";
+//    $conn = new mysqli($host, $username, $password,$db);
+    echo "Hello World";
+
+
+?>
---
+++ mainpage.php
@@ -0,0 +1,8 @@
+<?php
+
+    $main = 'Main';
+    $page = 'page';
+    $type = '.php';
+    echo $main.$page.$type;
+
+?>
